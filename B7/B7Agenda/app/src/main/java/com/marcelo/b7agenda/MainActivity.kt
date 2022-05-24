e7a6cca67aa095ae9cd04e39c5877ee3f6ae1118
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2022-05-24 09:48:52 +0200

    aaa

--- B7/B7Agenda/app/src/main/java/com/marcelo/b7agenda/MainActivity.kt
+++ B7/B7Agenda/app/src/main/java/com/marcelo/b7agenda/MainActivity.kt
@@ -2,6 +2,8 @@ package com.marcelo.b7agenda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.marcelo.b7agenda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val pagerAdapter = FragmentPageAdapter(this)
-        binding.ViewPager.adapter = pagerAdapter
+        val viewPager = binding.ViewPager
+        viewPager.adapter = pagerAdapter
+        val tabLayout = binding.tabs
+        TabLayoutMediator(tabLayout,viewPager){ tab,position->
+            when(position){
+                0-> tab.text = "Nuevo Contacto"
+                1-> tab.text = "Editar Contacto"
+            }
+        }.attach()
+
+
+
+
     }
 
     override fun onBackPressed() {
